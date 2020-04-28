b780dd99857da7184af99e5a20f8d61a6dd231d8
Seth Hoenig <shoenig@hashicorp.com>
2020-04-27 22:04:49 -0600

    wip build table of AWS CPU speeds

--- client/fingerprint/env_aws.go
+++ client/fingerprint/env_aws.go
@@ -21,14 +21,14 @@ import (
 
 const (
 	// AwsMetadataTimeout is the timeout used when contacting the AWS metadata
-	// service
+	// services.
 	AwsMetadataTimeout = 2 * time.Second
 )
 
 // map of instance type to approximate speed, in Mbits/s
 // Estimates from http://stackoverflow.com/a/35806587
 // This data is meant for a loose approximation
-var ec2InstanceSpeedMap = map[*regexp.Regexp]int{
+var ec2NetSpeedTable = map[*regexp.Regexp]int{
 	regexp.MustCompile("t2.nano"):      30,
 	regexp.MustCompile("t2.micro"):     70,
 	regexp.MustCompile("t2.small"):     125,
@@ -44,6 +44,61 @@ var ec2InstanceSpeedMap = map[*regexp.Regexp]int{
 	regexp.MustCompile(`.*\.10xlarge`): 10000,
 	regexp.MustCompile(`.*\.16xlarge`): 10000,
 	regexp.MustCompile(`.*\.32xlarge`): 10000,
+}
+
+// map of instance type to documented CPU speed, in KHz.
+//
+// Most values are taken from https://aws.amazon.com/ec2/instance-types/.
+// Values for a1 & m6g (Graviton) are taken from https://en.wikichip.org/wiki/annapurna_labs/alpine/al73400
+// Values for inf1 are taken from launching a inf1.xlarge and looking at /proc/cpuinfo
+//
+// In a few cases, AWS has upgraded the generation of CPU while keeping the same
+// instance designation. Since it is possible to launch on the lower performance
+// CPU, that one is used as the spec for the instance type.
+var ec2ProcSpeedTable = map[*regexp.Regexp]int{
+	// General Purpose
+	regexp.MustCompile(`a1\..*`):                              2300000, // Custom built AWS Graviton
+	regexp.MustCompile(`t3\..*`):                              2500000, // 2.5 GHz Intel Scalable
+	regexp.MustCompile(`t3a\..*`):                             2500000, // 2.5 GHz AMD EPYC 7000 series
+	regexp.MustCompile(`t2\.(nano)|(micro)|(small)|(medium)`): 3300000, // 3.3 GHz Intel Scalable
+	regexp.MustCompile(`t2\.(large)|(xlarge)|(2xlarge)`):      3000000, // 3.0 GHz Intel Scalable
+	regexp.MustCompile(`m6g\..*`):                             2300000, // Custom built AWS Graviton
+	regexp.MustCompile(`m5d?\..*`):                            3100000, // 3.1 GHz Intel Xeon Platinum
+	regexp.MustCompile(`m5ad?\..*`):                           2500000, // 2.5 GHz AMD EPYC 7000 series
+	regexp.MustCompile(`m5d?n\..*`):                           3100000, // 3.1 GHz Intel Xeon Scalable
+	regexp.MustCompile(`m4\..*`):                              2300000, // 2.3 GHz Intel Xeon® E5-2686 v4
+
+	// Compute Optimized
+	regexp.MustCompile(`c5d?\.(12xlarge)|(24xlarge)|(metal)`): 3600000, // 3.6 GHz Intel Xeon Scalable
+	regexp.MustCompile(`c5d?\..*`):                            3400000, // 3.4 GHz Intel Xeon Platinum 8000
+	regexp.MustCompile(`c5n\..*`):                             3000000, // 3.0 GHz Intel Xeon Platinum
+	regexp.MustCompile(`c4\..*`):                              2900000, // 2.9 GHz Intel Xeon E5-2666 v3
+
+	// Memory Optimized
+	regexp.MustCompile(`r5d?\..*`):                 3100000, // 3.1 GHz Intel Xeon Platinum
+	regexp.MustCompile(`r5ad\..*`):                 2500000, // 2.5 GHz AMD EPYC 7000 series
+	regexp.MustCompile(`r5d?n\..*`):                3100000, // 3.1 GHz Intel Xeon Scalable
+	regexp.MustCompile(`r4\..*`):                   2300000, // 2.3 GHz Intel Xeon E5-2686 v4
+	regexp.MustCompile(`x1e\..*`):                  2300000, // 2.3 GHz Intel Xeon E7-8880 v3
+	regexp.MustCompile(`x1\..*`):                   2300000, // 2.3 GHz Intel Xeon E7-8880 v3
+	regexp.MustCompile(`u-(6)|(9)|(12)tb1\.metal`): 2100000, // 2.1 GHz Intel Xeon Platinum 8176M
+	regexp.MustCompile(`u-(18)|(24)tb1\.metal`):    2700000, // 2.7 GHz Intel Xeon Scalable
+	regexp.MustCompile(`z1d\..*`):                  4000000, // 4.0 GHz Custom Intel Xeon Scalable
+
+	// Accelerated Computing
+	regexp.MustCompile(`p3\.(2xlarge)|(8xlarge)|(16xlarge)`): 2300000, // 2.3 GHz Intel Xeon E5-2686 v4
+	regexp.MustCompile(`p3\.24xlarge`):                       2500000, // 2.5 GHz Intel Xeon P-8175M
+	regexp.MustCompile(`p2\..*`):                             2300000, // 2.3 GHz Intel Xeon E5-2686 v4 Processor
+	regexp.MustCompile(`inf1\..*`):                           3000000, // 3.0 GHz Intel Xeon Platinum 8275CL
+	regexp.MustCompile(`g4dn\..*`):                           2500000, // 2.5 GHz Cascade Lake 24C
+	regexp.MustCompile(`g3s?\..*`):                           2300000, // 2.3 GHz Intel Xeon E5-2686 v4
+	regexp.MustCompile(`f1\..*`):                             2300000, // 2.3 GHz Intel Xeon E5-2686 v4
+
+	// Storage Optimized
+	regexp.MustCompile(`i3\..*`):   2300000, // 2.3 GHz Intel Xeon E5 2686 v4
+	regexp.MustCompile(`i3en\..*`): 3100000, // 3.1 GHz Intel Xeon Scalable
+	regexp.MustCompile(`d2\..*`):   2400000, // 2.4 GHz Intel Xeon E5-2676 v3
+	regexp.MustCompile(`h1\..*`):   2300000, // 2.3 GHz Intel Xeon E5 2686 v4
 }
 
 // EnvAWSFingerprint is used to fingerprint AWS metadata
@@ -147,6 +202,9 @@ func (f *EnvAWSFingerprint) Fingerprint(request *FingerprintRequest, response *F
 		}
 	}
 
+	// copy over CPU information
+	// response.Resources.CPU
+
 	// populate Links
 	response.AddLink("aws.ec2", fmt.Sprintf("%s.%s",
 		response.Attributes["platform.aws.placement.availability-zone"],
@@ -189,7 +247,7 @@ func (f *EnvAWSFingerprint) linkSpeed(ec2meta *ec2metadata.EC2Metadata) int {
 
 	key := strings.Trim(resp, "\n")
 	netSpeed := 0
-	for reg, speed := range ec2InstanceSpeedMap {
+	for reg, speed := range ec2NetSpeedTable {
 		if reg.MatchString(key) {
 			netSpeed = speed
 			break
